ca0bd997de433482356367714571d9bd680cb19c
unknown <imonbdcallingit@gmail.com>
2024-03-20 10:57:40 +0600

    Commit change

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -1,5 +1,4 @@
 class Config {
-  static String appId = 'ed39f4358d544ed5889981611a3320cf';
-  static String rtcToken =
-      '007eJxTYOD856ZrlOi36OARZ3W7j8rN69VuHjDY+trm69ZZ6b8v8hQrMKSmGFummRibWqSYmpikpphaWFhaWhiaGRomGhsbGSSnbQ35ldoQyMgwIaqPkZEBAkF8boa0nNKSktSiktTiEgYGAFDIIw8=';
+  static String appId = 'your app id';
+  static String rtcToken = 'your rtc token';
 }
